6b3c21bc862ee3fc3ba0c64abbfa05f2e092fe55
ARKAEA <rkacso20@student.aau.dk>
2022-10-11 14:50:01 +0200

    Added a test.

--- src/load-version2.py
+++ src/load-version2.py
@@ -1,14 +1,33 @@
-from json import load
+"""
 from keras.datasets import mnist
+from matplotlib import pyplot
+import pylab
 
-def load_data():
-    (train_X, train_y), (test_X, test_y) = mnist.load_data()
-"""    print('X_train: ' + str(train_X.shape))
-    print('Y_train: ' + str(train_y.shape))
-    print('X_test:  '  + str(test_X.shape))
-    print('Y_test:  '  + str(test_y.shape))
-    #hello
-    #join here
-""" 
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+print('X_train: ' + str(train_X.shape))
+print('Y_train: ' + str(train_y.shape))
+print('X_test:  '  + str(test_X.shape))
+print('Y_test:  '  + str(test_y.shape))
+ 
 
-load_data()
+for i in range(9):  
+    pyplot.subplot(330 + 1 + i)
+    pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
+
+print("yes")
+
+"""
+import matplotlib.pyplot as plt
+
+# Define Data
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Plot
+
+plt.plot(x, y)
+
+# Display
+
+plt.show()
