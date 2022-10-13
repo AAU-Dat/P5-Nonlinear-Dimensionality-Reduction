1c006457a6a6e69241ee9f1dde59254064b4dbf6
ARKAEA <rkacso20@student.aau.dk>
2022-10-13 16:03:53 +0200

    Added functionality to load mnist + plot. Missing tests

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
+import load_online
 import load 
 import pre_process 
+import numpy as np
 
 
 def main():
-    number_of_samples = 3
-    (training_data, test_data) = pre_process.load_mnist_all("../mnist-original-dataset/train-images-idx3-ubyte",
-                                                          "../mnist-original-dataset/train-labels-idx1-ubyte",
-                                                          "../mnist-original-dataset/t10k-images-idx3-ubyte",
-                                                          "../mnist-original-dataset/t10k-labels-idx1-ubyte", number_of_samples)
 
+    load_online.download_files()
+    number_of_samples = 1
+    (training_data, test_data) = pre_process.load_mnist_all("train_file_image",
+                                                          "train_file_label",
+                                                          "test_file_image",
+                                                          "test_file_label", number_of_samples)
+    
+    
 if __name__ == "__main__":
     main()
