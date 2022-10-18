8a34e5844964398b54f95f2c487e32fff1a1c3d0
ARKAEA <rkacso20@student.aau.dk>
2022-10-18 15:46:22 +0200

    A function which reduces data dims with isomap is achieved

--- src/isomap.py
+++ src/isomap.py
@@ -1,21 +1,16 @@
 from sklearn.manifold import Isomap
-"""
-Reduce the dimensionality of the data with Isomap
-    n_neighbors is the number of neighbors to consider for each point for the manifold approximation
-    n_components is the number of dimensions to reduce the data to
-"""
+
+# The most important parameters to consider are: data, n_neighbors and n_components
+
+# returns a n_components array of shape (n_samples, n_components)
 
 
-def reduce_isomap(data,
-                n_neighbors=5, 
-                n_components=2,
-                eigen_solver='auto', 
-                tol=0, # tolerance for stopping criterion
-                max_iter=None, # maximum number of iterations for the optimization
-                path_method='auto', # Djiakstra or Floyd-Warshall
-                neighbors_algorithm='auto', # algorithm to compute nearest neighbors
-                n_jobs=None, # number of parallel jobs to run, would be good idea to put it at a high number since isomap is slow
-                ):
+def reduce_data_with_isomap(data,
+                            n_neighbors=5,
+                            n_components=2,
+                            # number of parallel jobs to run; as isomap can be slow it is recommended to use some (or all)available cores
+                            n_jobs=None,
+                            ):
     isomap = Isomap(n_neighbors=n_neighbors, n_components=n_components)
-    isomap.fit(data)
-    return isomap.transform(data)
+
+    return isomap.fit_transform(data)
--- src/tests/test_isomap.py
+++ src/tests/test_isomap.py
@@ -10,6 +10,6 @@ def test_isomap_function():
     n_neighbors = 2
     n_components = 2
     data = [[1, 2,3], [5, 6,7], [7, 8,9]]
-    new_data = isomap.reduce_isomap(data=data, n_neighbors=n_neighbors, n_components=n_components)
+    new_data = isomap.reduce_data_with_isomap(data=data, n_neighbors=n_neighbors, n_components=n_components)
     assert new_data.shape == (3,2)
     
