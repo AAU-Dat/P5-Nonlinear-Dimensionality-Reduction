f434fbc8078bba2651f8b2fbfdb5f0cc5f04462f
sebastianbot6969 <saahol20@student.aau.dk>
2022-10-20 14:48:06 +0200

    made comments to the function and out commented the test function to confusion matrix, it should be ready for merge

--- src/confusion_matrix.py
+++ src/confusion_matrix.py
@@ -1,15 +1,15 @@
 from sklearn.metrics import confusion_matrix
-from sklearn import svm
-import load_mnist as load
+#from sklearn import svm
+#import load_mnist as load
 from sklearn.model_selection import train_test_split
 
-def test_create_confusion_matrix():
-    (training_data, test_data) = load.load_mnist()
-    model = svm.SVC(gamma=0.001)
-    model.fit(training_data[0], training_data[1])
-    cm = create_confusion_matrix(model)
+# def test_create_confusion_matrix():
+#     (training_data, test_data) = load.load_mnist()
+#     model = svm.SVC(gamma=0.001)
+#     model.fit(training_data[0], training_data[1])
+#     cm = create_confusion_matrix(model)
 
-
-def create_confusion_matrix(model, test_images, test_labels):
-    predictions = model.predict(test_images)
-    return confusion_matrix(test_labels, predictions)
+#creates a confusion matrix for for the model and returns it, needs the trained model and the test data
+def create_confusion_matrix(model, test):
+    predictions = model.predict(test[0])
+    return confusion_matrix(test[1], predictions)
