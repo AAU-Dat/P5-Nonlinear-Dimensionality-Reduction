d418a65586ca4c24f1fd84ebab27c01bccd522ce
sebastian <saahol20@student.aau.dk>
2022-10-21 08:56:06 +0200

    changed the variable naming and deleted comments

--- src/confusion_matrix.py
+++ src/confusion_matrix.py
@@ -1,15 +1,7 @@
 from sklearn.metrics import confusion_matrix
-#from sklearn import svm
-#import load_mnist as load
 from sklearn.model_selection import train_test_split
 
-# def test_create_confusion_matrix():
-#     (training_data, test_data) = load.load_mnist()
-#     model = svm.SVC(gamma=0.001)
-#     model.fit(training_data[0], training_data[1])
-#     cm = create_confusion_matrix(model)
-
 #creates a confusion matrix for for the model and returns it, needs the trained model and the test data
-def create_confusion_matrix(model, test):
-    predictions = model.predict(test[0])
-    return confusion_matrix(test[1], predictions)
+def create_confusion_matrix(model, test_data):
+    predictions = model.predict(test_data[0])
+    return confusion_matrix(test_data[1], predictions)
