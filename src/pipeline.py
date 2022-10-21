b1caad0291c0f869c44ddb1a4cd80228631500de
ARKAEA <rkacso20@student.aau.dk>
2022-10-21 09:49:55 +0200

    Changed pipeline so that it works and outputs conf matrx.

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,12 +1,12 @@
 import load_mnist
 import svm_model
 import confusion_matrix
-
+from sklearn import svm
 def main():
     #load the data
     (training_data, test_data) = load_mnist.load_mnist()
     #create the model
-    model = svm_model.create_model(training_data)
+    model = svm_model.train_model(training_data, svm.SVC())
     #create the confusion matrix
     print(confusion_matrix.create_confusion_matrix(model, test_data))
 
