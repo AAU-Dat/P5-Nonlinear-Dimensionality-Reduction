fc2fb632633b801d0052034df651f760346ec313
sebastianbot6969 <saahol20@student.aau.dk>
2022-10-25 13:33:05 +0200

    made the component parameter to default 2, and made the function so it returned the transformed data

--- src/pca.py
+++ src/pca.py
@@ -1,6 +1,6 @@
 from sklearn import decomposition
 #function to perform pca, takes in the data and the number of components and returns the data after pca
-def pca(data, n_components):
+def pca(data, n_components = 2):
     pca = decomposition.PCA(n_components)
-    new_data = pca.fit(data)
-    return new_data
+    fitted_data = pca.fit(data)
+    return fitted_data.transform(data)
