edff4491b5fc67a4ea1a09d59df237cc193f2640
ARKAEA <rkacso20@student.aau.dk>
2022-10-25 15:21:02 +0200

    New correct ratio added and tests too

--- src/validation_data.py
+++ src/validation_data.py
@@ -1,12 +1,19 @@
+#Validation from training data
+#Find the ratio beetween training and test
 def create_dev_set(data):
     (train_data, test_data) = data
     
-    number_of_train_samples = len(train_data[0])
+    number_of_train_samples = len(train_data[0]) 
 
-    ratio = 1/6
-    validation_size = int(number_of_train_samples * ratio)
+    number_of_test_samples = len(test_data[0]) 
+    
+    ratio = number_of_test_samples /( number_of_train_samples + number_of_test_samples)
+
+    validation_size = int(number_of_train_samples * ratio) 
 
     validation_data = (train_data[0][-validation_size:], train_data[1][-validation_size:])
+
+    train_data = (train_data[0][0:number_of_train_samples-validation_size], train_data[1][0:number_of_train_samples-validation_size])
 
     return (train_data, validation_data, test_data)
  
--- tests/validation_data_test.py
+++ tests/validation_data_test.py
@@ -1,16 +1,20 @@
+from math import ceil, floor
 import src.validation_data
 import src.load_mnist
 
 def test_create_new_data():
     (train_data, test_data) = src.load_mnist.load_mnist(60000)
     (train_data, validation_data, test_data) = src.validation_data.create_dev_set((train_data, test_data))
-    assert len(train_data[0]) == 60000
-    assert len(validation_data[0]) == 10000
+    assert len(train_data[0]) == ceil(6/7 * 60000)
+    assert len(validation_data[0]) == floor(1/7 * 60000) 
     assert len(test_data[0]) == 10000
 
 def test_create_new_data_with_10000_samples():
     (train_data, test_data) = src.load_mnist.load_mnist(10000)
+    ratio = (len(test_data[0]) / ((len(train_data[0]) + len(test_data[0]))))
+    samples = len(train_data[0])
     (train_data, validation_data, test_data) = src.validation_data.create_dev_set((train_data, test_data))
-    assert len(train_data[0]) == 10000
-    assert len(validation_data[0]) == 1666
+
+    assert len(train_data[0]) == int(ratio * samples)
+    assert len(validation_data[0]) == 5000
     assert len(test_data[0]) == 10000
