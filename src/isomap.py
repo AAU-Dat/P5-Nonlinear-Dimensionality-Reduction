7dab2fb0b4aad254c00c9f2a9fd8d5f70c6664f2
ARKAEA <rkacso20@student.aau.dk>
2022-10-25 09:24:43 +0200

    Isomap has the core functionality of reducing the data.

--- src/isomap.py
+++ src/isomap.py
@@ -1,16 +1,17 @@
 from sklearn.manifold import Isomap
+import numpy
+#import PCA
+from sklearn.decomposition import PCA
+
 
 # The most important parameters to consider are: data, n_neighbors and n_components
 
 # returns a n_components array of shape (n_samples, n_components)
+def reduced_data_with_isomap(data, n_components=2, n_neighbors=8):
+    isomap = Isomap(n_components=n_components)
+    return (isomap.fit_transform(data[0]), data[1])
 
-
-def reduce_data_with_isomap(data,
-                            n_neighbors=5,
-                            n_components=2,
-                            # number of parallel jobs to run; as isomap can be slow it is recommended to use some (or all)available cores
-                            n_jobs=None,
-                            ):
-    isomap = Isomap(n_neighbors=n_neighbors, n_components=n_components)
-
-    return isomap.fit_transform(data)
+#This function is made in case we want to get the object isomap, which is like the previous function but it returns the object isomap instead of returning the reduced data tuple
+def isomap_object_fitted_to_images(data, n_components=2):
+    isomap = Isomap(n_components=n_components)
+    return isomap.fit(data[0])
