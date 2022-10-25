1f66b879e774dd99ebee73987294205ba3679859
sebastianbot6969 <saahol20@student.aau.dk>
2022-10-25 11:53:24 +0200

    added comment to function and a variabe to return the function

--- src/pca.py
+++ src/pca.py
@@ -1,6 +1,6 @@
 from sklearn import decomposition
-
+#function to perform pca, takes in the data and the number of components and returns the data after pca
 def pca(data, n_components):
     pca = decomposition.PCA(n_components)
-    pca.fit(data)
-    return pca
+    new_data = pca.fit(data)
+    return new_data
