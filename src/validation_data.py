f66229ed189547d4c84dbd4da010ccc2b63fb088
ARKAEA <rkacso20@student.aau.dk>
2022-10-25 14:34:27 +0200

    Validation data now has a ratio

--- src/validation_data.py
+++ src/validation_data.py
@@ -1,24 +1,12 @@
-#Divide the training data set into a training and validation set
-
-def divide_data(data, validation_size=10000, ratio=0.8):
+def create_dev_set(data):
     (train_data, test_data) = data
+    
     number_of_train_samples = len(train_data[0])
 
-    ratio_percentage = int(number_of_train_samples * ratio)
+    ratio = 1/6
+    validation_size = int(number_of_train_samples * ratio)
 
-
-    #Validation set and test set have the same size if the we have less than 10000 samples
-    if number_of_train_samples <= validation_size:
-        validation_size = number_of_train_samples - ratio_percentage
-        test_data =(test_data[0][ratio_percentage:], test_data[1][ratio_percentage:])        
-        
-    #Validation set and test set have the same size so as to have the 80 % 20 % ratio between train and validation set
-    if (number_of_train_samples - ratio_percentage) < validation_size:
-        validation_size = number_of_train_samples - ratio_percentage
-        test_data =(test_data[0][-validation_size:], test_data[1][-validation_size:])
-    
     validation_data = (train_data[0][-validation_size:], train_data[1][-validation_size:])
 
-    train_data = (train_data[0][0:number_of_train_samples-validation_size], train_data[1][0:number_of_train_samples-validation_size])
-
     return (train_data, validation_data, test_data)
+ 
--- tests/validation_data_test.py
+++ tests/validation_data_test.py
@@ -1,37 +1,16 @@
 import src.validation_data
 import src.load_mnist
 
-def test_divide_data_for_all_data():
-    (train_data, test_data) = src.load_mnist.load_mnist()
-    (train_data, validation_data, test_data) = src.validation_data.divide_data((train_data, test_data))
-    assert len(train_data[0]) == 50000
+def test_create_new_data():
+    (train_data, test_data) = src.load_mnist.load_mnist(60000)
+    (train_data, validation_data, test_data) = src.validation_data.create_dev_set((train_data, test_data))
+    assert len(train_data[0]) == 60000
     assert len(validation_data[0]) == 10000
     assert len(test_data[0]) == 10000
 
-def test_divide_data_for_10000_data():
+def test_create_new_data_with_10000_samples():
     (train_data, test_data) = src.load_mnist.load_mnist(10000)
-    (train_data, validation_data, test_data) = src.validation_data.divide_data((train_data, test_data))
-    assert len(train_data[0]) == 8000
-    assert len(validation_data[0]) == 2000
-    assert len(test_data[0]) == 2000
-
-def test_divide_data_for_10000_data_with_different_ratio():
-    (train_data, test_data) = src.load_mnist.load_mnist(10000)
-    (train_data, validation_data, test_data) = src.validation_data.divide_data((train_data, test_data), ratio=0.5)
-    assert len(train_data[0]) == 5000
-    assert len(validation_data[0]) == 5000
-    assert len(test_data[0]) == 5000
-
-def test_divide_data_for_20000_data_with_different_ratio():
-    (train_data, test_data) = src.load_mnist.load_mnist(20000)
-    (train_data, validation_data, test_data) = src.validation_data.divide_data((train_data, test_data), ratio=0.5)
+    (train_data, validation_data, test_data) = src.validation_data.create_dev_set((train_data, test_data))
     assert len(train_data[0]) == 10000
-    assert len(validation_data[0]) == 10000
+    assert len(validation_data[0]) == 1666
     assert len(test_data[0]) == 10000
-
-def test_divide_data_with_20000_samples():
-    (train_data, test_data) = src.load_mnist.load_mnist(20000)
-    (train_data, validation_data, test_data) = src.validation_data.divide_data((train_data, test_data))
-    assert len(train_data[0]) == 16000
-    assert len(validation_data[0]) == 4000
-    assert len(test_data[0]) == 4000
