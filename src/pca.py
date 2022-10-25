0d48edaaca0a83f0f9a27bab8620dcd62746e014
sebastianbot6969 <saahol20@student.aau.dk>
2022-10-25 13:38:20 +0200

    amde the function to return a tuple instead of only one data

--- src/pca.py
+++ src/pca.py
@@ -1,6 +1,6 @@
 from sklearn import decomposition
 #function to perform pca, takes in the data and the number of components and returns the data after pca
 def pca(data, n_components = 2):
-    pca = decomposition.PCA(n_components)
-    fitted_data = pca.fit(data)
-    return fitted_data.transform(data)
+    pca = decomposition.PCA(n_components = n_components)
+    fitted_data = pca.fit(data[0])
+    return (fitted_data.transform(data), data[1])   
