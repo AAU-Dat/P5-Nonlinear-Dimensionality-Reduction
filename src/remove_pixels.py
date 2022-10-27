d2aa74a1ff6850c73bad0eb1750983fa70470023
sebastianbot6969 <saahol20@student.aau.dk>
2022-10-27 11:34:54 +0200

    made the function to only find black pixels

--- src/remove_pixels.py
+++ src/remove_pixels.py
@@ -3,17 +3,13 @@ import numpy as np
 import random
 
 #make function to remove random spaces from matrix and returns a new matrix with the spaces removed
-def remove_pixels(matrix, procent = 10):
-    #make a list of random numbers
+def remove_pixels(matrix, number_of_pixels = 70):
     new_matrix = matrix.copy()
-    random_list = []
-    number_of_pixels = int((procent / 100) * 784)
-    for i in range(0, number_of_pixels):
-        random_list.append(random.randint(0, 783))
-    
-    #removes dublikates
-    random_list = list(set(random_list))
-    
-    for i in range(0, len(random_list)):
-        new_matrix[random_list[i]] = 0
+    count = 0
+    while count < number_of_pixels:
+        #get random number
+        random_number = random.randint(0, 784)
+        if new_matrix[random_number] == 255 and count < number_of_pixels:
+            new_matrix[random_number] = 0
+            count += 1
     return new_matrix
