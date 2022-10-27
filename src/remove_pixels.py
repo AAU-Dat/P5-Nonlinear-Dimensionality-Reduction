2735e943d3ff59bb9a1159eea3e67cb8093d770f
sebastianbot6969 <saahol20@student.aau.dk>
2022-10-27 10:10:06 +0200

    the function now returns a new matrix with the values

--- src/remove_pixels.py
+++ src/remove_pixels.py
@@ -1,16 +1,19 @@
+from textwrap import fill
 import numpy as np
 import random
 
-#make function to remove random spaces from matrix and return the matrix
+#make function to remove random spaces from matrix and returns a new matrix with the spaces removed
 def remove_pixels(matrix, procent = 10):
     #make a list of random numbers
+    new_matrix = matrix.copy()
     random_list = []
     number_of_pixels = int((procent / 100) * 784)
     for i in range(0, number_of_pixels):
         random_list.append(random.randint(0, 783))
     
+    #removes dublikates
     random_list = list(set(random_list))
     
     for i in range(0, len(random_list)):
-        matrix[random_list[i]] = 0
-    return matrix
+        new_matrix[random_list[i]] = 0
+    return new_matrix
