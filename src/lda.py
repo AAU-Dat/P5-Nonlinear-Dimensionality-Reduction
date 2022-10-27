7982fff90c0f1bed3f501f24c0ba0dfc956ffce9
Actam55 <l.1999@hotmail.dk>
2022-10-27 15:40:05 +0200

    finished lda and works with current pipeline

--- src/lda.py
+++ src/lda.py
@@ -1,16 +1,23 @@
-import load_mnist
-import validation_data
-import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-def lda():
-    (train_data, val_data, test_data) = validation_data.divide_data(load_mnist.load_mnist())
-    clf = LinearDiscriminantAnalysis()
-    clf.fit_transform(train_data[0], train_data[1])
-    print(clf.score(val_data[0], val_data[1]))
-
-lda()
-
+def lda(data):
+    #data is a tuple of (training_data, test_data)
+    (train_data, test_data) = data
+    #creates the LDA model
+    lda = LinearDiscriminantAnalysis()
+    
+    #Fits training data to the lda model
+    fitted_train_data = lda.fit(train_data[0], train_data[1])
+    #Transforms the training data to the new space
+    lda_train_data = (fitted_train_data.transform(train_data[0]), train_data[1])
+    
+    #fits the test data to the lda model
+    fitted_test_data = lda.fit(test_data[0], test_data[1])
+    #Transforms the test data to the new space
+    lda_test_data = (fitted_test_data.transform(test_data[0]), test_data[1])
+    #returns the transformed data as a tuple
+    
+    return (lda_train_data, lda_test_data)
 
 
 
