05a3c8d7ad24329ba0aaeb1e82a132116e68cf05
ARKAEA <rkacso20@student.aau.dk>
2022-10-28 15:23:43 +0200

    Added time to the pipeline, and tuples for the metrics for training and testing images

--- src/dim_evaluation.py
+++ src/dim_evaluation.py
@@ -3,12 +3,25 @@ from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
 import time
 import numpy as np
-    
-def return_metrics_for_data(data, labels, start=time.time()):
-    #we are not interested in the time it takes to compute without dim red
-    if data.shape[1] < 784:    
-        print("Elapsed time for dim. red: %.2f s" % (end_time(start)))
 
+
+
+def take_time(data, start=time.time()):
+    return data, end_time(start)
+
+def give_metrics_for_data(data):
+    train_instance = data[0]
+    test_instance = data[1]
+
+    (shape_train, average_class_train, average_total_train, cluster_train) = return_metrics_for_data(train_instance[0], train_instance[1])
+    train_metric = (shape_train, average_class_train, average_total_train, cluster_train)
+    
+    (shape_test, average_class_test, average_total_test, cluster_test) = return_metrics_for_data(test_instance[0], test_instance[1])
+    test_metric = (shape_test, average_class_test, average_total_test, cluster_test)
+    
+    return (train_metric, test_metric)
+
+def return_metrics_for_data(data, labels, start=time.time()):
     data_shape = data.shape
     average_distance_for_data = average_distance(data)
     average_distance_between_classes_for_data = average_distance_between_classes(data, labels)
