9ed773d763f37dab8a7bdd513333d1141f86025e
Actam55 <l.1999@hotmail.dk>
2022-11-03 16:04:05 +0100

    missing for multiple images and data types are fucked

--- src/remove_pixels.py
+++ src/remove_pixels.py
@@ -5,19 +5,73 @@ import random
 
 def remove_datapoints(data, num_of_datapoints):
     (training_data, test_data) = data
-    picture_one = training_data[0][0]
-    for i in range(num_of_datapoints):
-        picture_one = remove_random_pixel(picture_one)
-    return (picture_one, training_data[1][0])
-        
+    list_training_data_img = list(training_data[0][0])
+    i = 0
+    while i < num_of_datapoints:
+        random_index = random.randint(0, len(list_training_data_img) - 1)
+        if (list_training_data_img[random_index] > 0):
+            list_training_data_img[random_index] = 0
+            i += 1
+    training_data = (np.array(list_training_data_img), training_data[1][0])
+    return (training_data, test_data)
 
-def remove_random_pixel(data):
-    pixel = data[random.randint(0, 783)]
-    if(pixel > 0):
-        pixel = 0
-    else:
-        remove_random_pixel(data)
-    return data
+# def remove_datapoints(data, num_of_datapoints):
+#     (training_data, test_data) = data
+#     list_training_data_img = list(training_data[0][0])
+#     matrix = np.reshape(list_training_data_img, (28, 28))
+#     num = [random.randint(0, 27), random.randint(0, 27)]
+#     surrounding_matrix = surrounding(matrix, num, 1, None)
+#     i = 0
+#     # while i < num_of_datapoints:
+#     #     random_index = [random.randint(0, 27), random.randint(0, 27)]
+#     #     if (matrix[random_index[0], random_index[1]] > 0):
+#     #         matrix[random_index[0], random_index[1]] = 0
+            
+#     #         i += 1
+#     training_data = tuple(list_training_data_img)
+#     return (training_data, test_data)
+
+# def surrounding(x, idx, radius=1, fill=0):
+#     """ 
+#     Gets surrounding elements from a numpy array 
+  
+#     Parameters: 
+#     x (ndarray of rank N): Input array
+#     idx (N-Dimensional Index): The index at which to get surrounding elements. If None is specified for a particular axis,
+#         the entire axis is returned.
+#     radius (array-like of rank N or scalar): The radius across each axis. If None is specified for a particular axis, 
+#         the entire axis is returned.
+#     fill (scalar or None): The value to fill the array for indices that are out-of-bounds.
+#         If value is None, only the surrounding indices that are within the original array are returned.
+  
+#     Returns: 
+#     ndarray: The surrounding elements at the specified index
+#     """
+    
+#     assert len(idx) == len(x.shape)
+    
+#     if np.isscalar(radius):
+#         radius = tuple([radius for i in range(len(x.shape))])
+#     slices = []
+#     paddings = []
+#     for axis in range(len(x.shape)):
+#         if idx[axis] is None or radius[axis] is None:
+#             slices.append(slice(0, x.shape[axis]))
+#             paddings.append((0, 0))
+#             continue
+            
+#         r = radius[axis]
+#         l = idx[axis] - r 
+#         r = idx[axis] + r
+        
+#         pl = 0 if l > 0 else abs(l)
+#         pr = 0 if r < x.shape[axis] else r - x.shape[axis] + 1
+        
+#         slices.append(slice(max(0, l), min(x.shape[axis], r+1)))
+#         paddings.append((pl, pr))
+
+#     return x[slices]
+
 
 # def remove_pixels(data, number_of_pixels = 10):
 #     #call remove_pixels_from_image for each image in the data and return the new data tuble
@@ -49,8 +103,8 @@ def remove_random_pixel(data):
 #     return new_matrix
 
 # def change_tuple_firstvalue (data, new_data):
-#     new_training_data_tuble = (new_data, data[0][1])
-#     new_tuble = (new_training_data_tuble, data[1])
+#     list_training_data_img_tuble = (new_data, data[0][1])
+#     new_tuble = (list_training_data_img_tuble, data[1])
 #     return new_tuble
 
 
